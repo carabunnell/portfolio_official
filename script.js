764932f78aa4b4286bb6f47e9f201bc3522144ee
Cara Bunnell <carabunnell@gmail.com>
2020-02-22 19:08:27 -0800

    inserting links to professional materials in html

---
+++ script.js
@@ -0,0 +1,37 @@
+// $(document).ready(function() {
+//     console.log("hello");
+//     var resume = "assets/documents/resume_cara_lydia.pdf";
+// var github = "https://github.com/carabunnell";
+// var linkedin = "https://www.linkedin.com/in/cara-bunnell-734442aa";
+// var talkGeeky = "https://carabunnell.github.io/project_one_ucsd/";
+// var weather = "https://carabunnell.github.io/weather_dashboard/";
+// var workday = "https://carabunnell.github.io/work_day_scheduler/";
+
+// $("link").on("click", function() {
+//     var userChoice = "Resume"; // $("link").html();
+//     console.log(userChoice);
+//     if (userChoice = "Resume") {
+//         function openTab(th)
+//             {
+//                 window.open(th.name,'_blank');
+//             }
+//     }
+//     else if (userChoice === "GitHub") {
+//         console.log(github);
+//         // "https://github.com/carabunnell";
+//     }
+//     else if (userChoice === "LinkedIn") {
+//         //open linkedin profil in seprate doc
+//     }
+//     else if (userChoice === "Talk Geeky to Me") {
+//         //open github profil in seprate doc
+//     }
+//     else if (userChoice === "Weather Dashboard") {
+//         //open github profil in seprate doc
+//     }
+//     else if (userChoice === "Work Day Scheduler") {
+//         //open github profil in seprate doc
+//     }
+// });
+// });
+
